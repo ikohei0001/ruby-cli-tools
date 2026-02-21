9a6785efdfffc60567f5710b668c9ecd1e8aeac7
ikohei0001 <labellavita140@gmail.com>
2026-02-21 19:37:33 +0900

    fix filetool.rb

--- lib/filetool.rb
+++ lib/filetool.rb
@@ -1,122 +1,116 @@
 require "tempfile"
 
 module Filetool
-  class << self
-    def show(filename)
-      File.read(filename)
-    end
-  
-    def stats(filename)
-      lines = 0
-      words = 0
-      chars = 0
+  module_function
+  def show(path)
+    File.read(path)
+  end
 
-      File.foreach(filename) do |line|
-        lines += 1
-        words += line.split.size
-        chars += line.length
+  def stats(path)
+    content = File.read(path)
+
+    {
+      lines: content.lines.count,
+      words: content.split.size,
+      chars: content.length
+    }
+  end
+
+  def add(filename, text)
+    File.open(filename, 'a') { |f| f.puts text }
+  end
+
+  def search(path, string, options = {})
+    results = []
+    
+    flags = options[:ignore_case] ? Regexp::IGNORECASE : nil
+    pattern = Regexp.new(Regexp.escape(string), flags)
+
+    File.foreach(path).with_index(1) do |line, no|
+      if line.match?(pattern)
+        if options[:count]
+          results << no
+        else
+          results << "#{no}: #{line}"
+        end
       end
-
-      { lines: lines, words: words, chars: chars }
     end
 
-    def add(filename, text)
-      File.open(filename, 'a') { |f| f.puts text }
+    if options[:count]
+      return results.size
     end
 
-    def search(filename, string, options = {})
-      results = []
-      
-      pattern = options[:ignore_case] ? /#{Regexp.escape(string)}/i : string
+    results
+  end
 
+  def replace!(filename, from, to)
+    results = []
+
+    Tempfile.create do |tmp|
       File.foreach(filename).with_index(1) do |line, no|
-        if line.match?(pattern)
-          if options[:count]
-            results << no
-          else
-            results << "#{no}: #{line}"
-          end
+        if line.match?(from)
+          replaced = line.gsub(from, to)
+          results << "#{no}: #{replaced.chomp}"
+          tmp.write(replaced)
+        else
+          tmp.write(line)
         end
       end
 
-      if options[:count]
-        return results.size
-      end
-
-      results
+      tmp.flush
+      File.write(filename, File.read(tmp.path))
     end
+    results
+  end
 
-    def replace!(filename, from, to)
-      results = []
-
-      Tempfile.create do |tmp|
-        File.foreach(filename).with_index(1) do |line, no|
-          if line.match?(from)
-            replaced = line.gsub(from, to)
-            results << "#{no}: #{replaced.chomp}"
-            tmp.write(replaced)
-          else
-            tmp.write(line)
-          end
-        end
-
-        tmp.flush
-        File.write(filename, File.read(tmp.path))
-      end
-      results
+  def create(filename)
+    if File.exist?(filename)
+      raise "File already exists"
     end
+    File.write(filename, "")
+  end
 
-    def create(filename)
-      if File.exist?(filename)
-        raise "File already exists"
-      end
-      File.write(filename, "")
+  def delete(filename)
+    raise "File not found" unless File.exist?(filename)
+    File.delete(filename)
+  end
+
+  def rename(old_name, new_name)
+    raise "File not found #{old_name}" unless File.exist?(old_name)
+    raise "File already exist: #{new_name}" if File.exist?(new_name)
+
+    File.rename(old_name, new_name)
+  end
+
+  def mkdir(dirname)
+    raise "Directory exist: #{dirname}" if Dir.exist?(dirname)
+
+    Dir.mkdir(dirname)
+  end
+
+  def rmdir(dirname)
+    raise "No such directory: #{dirname}" unless Dir.exist?(dirname)
+    raise "Directory is not empty: #{dirname}" unless Dir.empty?(dirname)
+
+    Dir.rmdir(dirname)
+  end
+
+  def ls(dirname)
+    raise "No such directory: #{dirname}"  unless Dir.exist?(dirname)
+
+    Dir.children(dirname).map do |name|
+      path = File.join(dirname, name)
+      format("%-12s%s", File.directory?(path) ? "Directory:" : "", name)
     end
+  end
 
-    def delete(filename)
-      unless File.exist?(filename)
-        return "File not found"
-      end
-      File.delete(filename)
-    end
+  def pwd
+    Dir.pwd
+  end
 
-    def rename(old_name, new_name)
-      raise "File not found #{old_name}" unless File.exist?(old_name)
-      raise "File already exist: #{new_name}" if File.exist?(new_name)
-
-      File.rename(old_name, new_name)
-    end
-
-    def mkdir(dirname)
-      raise "Directory exist: #{dirname}" if Dir.exist?(dirname)
-
-      Dir.mkdir(dirname)
-    end
-
-    def rmdir(dirname)
-      raise "No such directory: #{dirname}" unless Dir.exist?(dirname)
-      raise "Directory is not empty: #{dirname}" unless Dir.empty?(dirname)
-
-      Dir.rmdir(dirname)
-    end
-
-    def ls(dirname)
-      raise "No such directory: #{dirname}"  unless Dir.exist?(dirname)
-
-      Dir.children(dirname).map do |name|
-        path = File.join(dirname, name)
-        format("%-12s%s", File.directory?(path) ? "Directory:" : "", name)
-      end
-    end
-
-    def pwd
-      Dir.pwd
-    end
-
-    def cd(path)
-      Dir.chdir(path)
-      Dir.pwd
-    end
+  def cd(path)
+    Dir.chdir(path)
+    Dir.pwd
   end
 end
 
